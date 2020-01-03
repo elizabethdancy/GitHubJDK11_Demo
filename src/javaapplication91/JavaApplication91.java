bd5926dd26d03999ec137602b1a98ef1e59d0e32
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2020-01-03 10:39:17 -0500

    revert

--- src/javaapplication91/JavaApplication91.java
+++ src/javaapplication91/JavaApplication91.java
@@ -15,7 +15,7 @@ public class JavaApplication91 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Edited on GitHub");
+        // TODO code application logic here
     }
     
 }
