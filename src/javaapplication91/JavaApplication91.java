cec73e7e3b45ec2f8e9df275f02cd19f2d82bcb8
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2020-01-03 10:41:39 -0500

    Revert "Update JavaApplication91.java"
    This reverts commit f38e1c23b370026d32ea3e26b18a1989222483d1.

--- src/javaapplication91/JavaApplication91.java
+++ src/javaapplication91/JavaApplication91.java
@@ -15,7 +15,7 @@ public class JavaApplication91 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("New Change on GitHub 2");
+        // TODO code application logic here
     }
     
 }
